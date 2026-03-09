e896898def795bbcf4801a39cb8bdd953200e05c
kimjisub <0226daniel@gmail.com>
2026-03-09 11:50:13 +0900

    Initial Commit

---
+++ unipadTests/unipadTests.swift
@@ -0,0 +1,16 @@
+//
+//  unipadTests.swift
+//  unipadTests
+//
+//  Created by 김지섭 on 3/9/26.
+//
+
+import Testing
+
+struct unipadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ unipadUITests/unipadUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  unipadUITestsLaunchTests.swift
+//  unipadUITests
+//
+//  Created by 김지섭 on 3/9/26.
+//
+
+import XCTest
+
+final class unipadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
